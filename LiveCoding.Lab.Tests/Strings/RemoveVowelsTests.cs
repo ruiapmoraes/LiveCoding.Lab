6baed6067734570cead3d5ecac4fbf77e1ecfcc2
Rui Moraes <rui-moraes@live.com>
2026-01-30 19:08:13 -0300

    Adiciona testes unitários para remoção de vogais
    Remove a pasta "Strings" do projeto de testes e exclui UnitTest1.cs. Adiciona RemoveVowelsTests.cs com casos de teste para o método RemoveVowels.Execute, cobrindo remoção de vogais, string vazia e vogais maiúsculas.

---
+++ LiveCoding.Lab.Tests/Strings/RemoveVowelsTests.cs
@@ -0,0 +1,36 @@
+﻿using LiveCoding.Lab.Core.Exercises.Strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiveCoding.Lab.Tests.Strings
+{
+    public class RemoveVowelsTests
+    {
+        [Fact]
+        public void Should_Remove_Vowels_From_String()
+        {
+            // Arrange
+            var input = "Hello, Live Coding Lab!";
+            var result = RemoveVowels.Execute(input); 
+            
+            Assert.Equal("Hll, Lv Cdng Lb!", result);
+        }
+        
+        [Fact]
+        public void Should_Return_Empty_When_Input_Is_Empty()
+        {
+            var result = RemoveVowels.Execute("");
+
+            Assert.Equal("", result);
+        }
+
+        [Fact]
+        public void Should_Work_With_Uppercase_Vowels()
+        {
+            var result = RemoveVowels.Execute("AEIOU are vowels");
+
+            Assert.Equal(" r vwls", result);
+        }
+    }
+}
